442f6b57dd49330216a792193d934f156f8b3d57
wolfun89 <86215697+wolfun89@users.noreply.github.com>
2022-03-30 14:22:13 +0200

    Yes please

---
+++ src/books.js
@@ -0,0 +1,22 @@
+export const books = [
+  {
+    id: 1,
+    img: "https://images-na.ssl-images-amazon.com/images/I/8144Vic9C5L._AC_UL302_SR302,200_.jpg",
+    title: "I Love You To the Moon and Back",
+    author: "Amelia Hepworth"
+  },
+
+  {
+    id: 2,
+    img: "https://m.media-amazon.com/images/I/71aLultW5EL._AC_UY327_FMwebp_QL65_.jpg",
+    title: "Our Class is a Family",
+    author: "Shannon Olsen"
+  },
+
+  {
+    id: 3,
+    img: "https://m.media-amazon.com/images/I/61B7tFxkYFS.jpg",
+    title: "The Vanishing Half: A Novel",
+    author: "Brit Bennett"
+  }
+]
